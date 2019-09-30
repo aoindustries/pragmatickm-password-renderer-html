eaee553791dba4989ebc0d3de0ac164299014a41
AO Industries, Inc. <support@aoindustries.com>
2019-09-30 09:47:21 -0500

    Updated dependencies.

--- src/main/java/com/pragmatickm/password/renderer/html/PasswordTableHtmlRenderer.java
+++ src/main/java/com/pragmatickm/password/renderer/html/PasswordTableHtmlRenderer.java
@@ -28,6 +28,8 @@ import static com.aoindustries.encoding.TextInXhtmlAttributeEncoder.encodeTextIn
 import static com.aoindustries.encoding.TextInXhtmlAttributeEncoder.textInXhtmlAttributeEncoder;
 import static com.aoindustries.encoding.TextInXhtmlEncoder.encodeTextInXhtml;
 import com.aoindustries.io.buffer.BufferResult;
+import com.aoindustries.servlet.ServletUtil;
+import com.aoindustries.servlet.URIComponent;
 import com.aoindustries.servlet.http.LastModifiedServlet;
 import com.pragmatickm.password.model.Password;
 import com.pragmatickm.password.model.PasswordTable;
@@ -82,7 +84,6 @@ final public class PasswordTableHtmlRenderer {
 			if(childElement instanceof Password) allPasswords.add((Password)childElement);
 		}
 
-		final String responseEncoding = response.getCharacterEncoding();
 		// Find which columns need to be displayed
 		boolean hasHref = false;
 		Set<String> uniqueCustomFields = new LinkedHashSet<>();
@@ -271,17 +272,21 @@ final public class PasswordTableHtmlRenderer {
 									if(element == null) {
 										if(index != null) {
 											out.write('#');
-											PageIndex.appendIdInPage(
-												index,
-												null,
-												new MediaWriter(textInXhtmlAttributeEncoder, out)
+											URIComponent.FRAGMENT.encode(
+												PageIndex.getRefId(
+													index,
+													null
+												),
+												response,
+												out,
+												textInXhtmlAttributeEncoder
 											);
 										} else {
 											encodeTextInXhtmlAttribute(
 												response.encodeURL(
-													com.aoindustries.net.UrlUtils.encodeUrlPath(
+													ServletUtil.encodeURI(
 														request.getContextPath() + bookRef.getPrefix() + pageRef.getPath(),
-														responseEncoding
+														response
 													)
 												),
 												out
@@ -290,17 +295,25 @@ final public class PasswordTableHtmlRenderer {
 									} else {
 										if(index != null) {
 											out.write('#');
-											PageIndex.appendIdInPage(
-												index,
-												element,
-												new MediaWriter(textInXhtmlAttributeEncoder, out)
+											URIComponent.FRAGMENT.encode(
+												PageIndex.getRefId(
+													index,
+													element
+												),
+												response,
+												out,
+												textInXhtmlAttributeEncoder
 											);
 										} else {
 											encodeTextInXhtmlAttribute(
 												response.encodeURL(
-													com.aoindustries.net.UrlUtils.encodeUrlPath(
-														request.getContextPath() + bookRef.getPrefix() + pageRef.getPath() + '#' + element,
-														responseEncoding
+													ServletUtil.encodeURI(
+														request.getContextPath()
+														+ bookRef.getPrefix()
+														+ pageRef.getPath()
+														+ '#'
+														+ URIComponent.FRAGMENT.encode(element, response),
+														response
 													)
 												),
 												out
